0d0018e9fa674c3716f801ab0849461634446a47
Jani Turunen <janisku7@gmail.com>
2019-11-22 23:30:22 +0200

    Add project files.

---
+++ SKUMassEffectCodex/SKUMassEffectCodex/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SKUMassEffectCodex.Models;
+
+namespace SKUMassEffectCodex.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
